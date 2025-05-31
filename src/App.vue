23ea43a7f476ed2f15a5d9bc57c295747ee8e09c
moyan <1561515308@qq.com>
2025-05-31 21:43:48 +0800

    build(dependencies): 添加 Element Plus 组件库
    - 在 package.json 中添加 element-plus 和 unplugin-element-plus 依赖
    - 在 vite.config.ts 中引入 ElementPlus 插件
    - 在 App.vue 中添加 ElButton 组件示例

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import { ElButton } from 'element-plus';
 </script>
 
 <template>
+    <ElButton type="primary">
+        <RouterLink to="/">Home</RouterLink>
+    </ElButton>
     <RouterView />
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import ElementPlus from 'unplugin-element-plus/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
         tailwindcss(),
+        ElementPlus(),
     ],
     resolve: {
         alias: {
